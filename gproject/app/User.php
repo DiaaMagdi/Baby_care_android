668395082dda8b4a3e6e5ce146f33e417a20ccf5
Asmaa Ibrahim <asmaa.elajamy@yahoo.com>
2020-03-12 21:00:07 +0200

    First commit

---
+++ gproject/app/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use app\baby;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table ="users";
+    protected $fillable = [
+        'name',
+        'email',
+        'gender',
+        'type',
+        'role',
+        'password',
+        'api_token'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token','api_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function baby(){
+        //return $this->hasMany(baby::class);
+        return $this->belongsTo('app\baby');
+    }
+}
