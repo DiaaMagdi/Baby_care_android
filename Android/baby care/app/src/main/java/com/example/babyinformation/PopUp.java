e0188aba0a48e0db42de89c15db2418cec8f4ca3
MariamEldoweny <mariam.eldoweny.2016@gmail.com>
2020-03-15 00:00:58 +0200

    choosing doctors and pop-up activity

---
+++ Android/baby care/app/src/main/java/com/example/babyinformation/PopUp.java
@@ -0,0 +1,23 @@
+package com.example.babyinformation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class PopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8),(int)(height*.6));
+    }
+}
