54c4ac244d6af19128ee97df3b27c6efa1f3beb5
MariamEldoweny <mariam.eldoweny.2016@gmail.com>
2020-03-17 02:28:27 +0200

    SlidePager

--- Android/baby care/app/src/main/java/com/example/babyinformation/PopUp.java
+++ Android/baby care/app/src/main/java/com/example/babyinformation/PopUp.java
@@ -1,11 +1,24 @@
 package com.example.babyinformation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.babyinformation.fragments.PageFragment1;
+import com.example.babyinformation.fragments.PageFragment2;
+import com.example.babyinformation.fragments.PageFragment3;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PopUp extends AppCompatActivity {
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,15 @@ public class PopUp extends AppCompatActivity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.8),(int)(height*.6));
+        getWindow().setLayout((int) (width*.95),(int)(height*.6));
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new PageFragment1());
+        list.add(new PageFragment2());
+        list.add(new PageFragment3());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(),list);
+        pager.setAdapter(pagerAdapter);
     }
 }
