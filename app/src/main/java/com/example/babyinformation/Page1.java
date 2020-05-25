81def2dc93a85b89af44819e5e28c81f8522a240
Amr <amrfearless>
2020-05-25 20:47:12 +0200

    Clean version without filtering

--- app/src/main/java/com/example/babyinformation/Page1.java
+++ app/src/main/java/com/example/babyinformation/Page1.java
@@ -1,13 +1,11 @@
 package com.example.babyinformation;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Page1 extends AppCompatActivity {
-
 
 
     @Override
@@ -15,16 +13,6 @@ public class Page1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
 
-    }
-
-    public void Location(View view) {
-        Intent intent = new Intent(this, Page2.class);
-        startActivity(intent);
-    }
-
-    public void Speciality(View view) {
-        Intent intent = new Intent(this, Page3.class);
-        startActivity(intent);
     }
 }
 
