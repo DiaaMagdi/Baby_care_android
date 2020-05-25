5cafd13d14b754446c4576cdce9c2d4fb6555e09
Amr <amrfearless>
2020-05-25 19:25:14 +0200

    added more files

---
+++ app/src/main/java/com/example/babyinformation/Doctors_Client.java
@@ -0,0 +1,31 @@
+package com.example.babyinformation;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Doctors_Client {
+    private static final String BASE_URL="https://next.json-generator.com/";
+    private Doctors_interface Doctors_interface;
+    private static Doctors_Client INSTANCE;
+
+
+    public Doctors_Client(){
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        Doctors_interface= retrofit.create(Doctors_interface.class);
+    }
+
+
+    public static Doctors_Client getINSTANCE() {
+        if (null == INSTANCE){
+            INSTANCE = new Doctors_Client();
+        }
+        return INSTANCE;
+    }
+    public Call<List<DoctorsDatabase>> getDoctors()
+    {
+        return Doctors_interface.getDoctors();
+    }
+}
