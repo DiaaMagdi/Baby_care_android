60bd0d4e0b1e74365eee44d42e62528d1a22be04
Amr <amrfearless>
2020-05-25 20:48:08 +0200

    Clean version without filtering

---
+++ app/src/main/java/com/example/babyinformation/Doctors_ViewModel.java
@@ -0,0 +1,35 @@
+package com.example.babyinformation;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Doctors_ViewModel extends ViewModel {
+    MutableLiveData<List<DoctorsDatabase>> DoctorsMutableData = new MutableLiveData<>();
+
+
+
+    public Doctors_ViewModel() {
+
+        Log.v("development", "callingAPI");
+
+        Doctors_Client.getINSTANCE().getDoctors().enqueue(new Callback<List<DoctorsDatabase>>() {
+            @Override
+            public void onResponse(Call<List<DoctorsDatabase>> call, Response<List<DoctorsDatabase>> response) {
+                DoctorsMutableData.setValue(response.body());
+                Log.v("development", "success");
+            }
+
+            @Override
+            public void onFailure(Call<List<DoctorsDatabase>> call, Throwable t) {
+                Log.v("development", t.getMessage());
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/babyinformation/Doctors_interface.java
@@ -0,0 +1,12 @@
+package com.example.babyinformation;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Doctors_interface {
+    @GET("api/json/get/EJQ7fk4jd")
+    Call<List<DoctorsDatabase>> getDoctors();
+
+}
